99291e89b45f9028f87c6b0cbd85eae9cb277707
miguelmdaguiar <mmaro3@iscte-iul.pt>
2025-10-29 15:38:16 +0100

    Move Method e Rename v6 e v7

--- src/v0/Rental.java
+++ src/v0/Rental.java
@@ -2,7 +2,7 @@ package v0;
 
 public class Rental
 {
-	private Movie	_movie;
+	Movie	_movie;
 	private int		_daysRented;
 
 	public Rental(Movie movie, int daysRented)
@@ -20,34 +20,4 @@ public class Rental
 	{
 		return _movie;
 	}
-
-    public double getAmount() {
-        double thisAmount = 0;
-
-        // determine amounts for each line
-        switch (getMovie().getPriceCode())
-        {
-            case REGULAR:
-                thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
-                break;
-            case CHILDRENS:
-                thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
-
-    public int getFrequentRentalPoints() {
-
-        return (getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && getDaysRented() > 1 ? 2 : 1;
-
-    }
-
 }
