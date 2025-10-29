6bec374b6bd1bcce36798a273009df9a787abc7f
miguelmdaguiar <mmaro3@iscte-iul.pt>
2025-10-29 15:23:09 +0100

    refactor Replace Temp with Query v4

--- src/v0/Customer.java
+++ src/v0/Customer.java
@@ -24,25 +24,27 @@ public class Customer
 
 	public String statement()
 	{
-		double totalAmount = 0;
-        int frequentRenterPoints = 0;
-
-		// header
+        // header
 		String result = "Rental Record for " + getName() + "\n";
-		
-		for (Rental each: _rentals)
-		{
-            frequentRenterPoints += each.getFrequentRentalPoints();
-
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
-			totalAmount += each.getAmount();
-		}
 
 		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
+		result += "Amount owed is " + getTotalAmount() + "\n";
+		result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
 		return result;
 	}
+    public int getTotalFrequentRenterPoints()
+    {
+        int frequentRenterPoints = 0;
+        for (Rental each: _rentals)
+            frequentRenterPoints += each.getFrequentRentalPoints();
+        return frequentRenterPoints;
+    }
 
+    public getTotalAmount()
+    {
+        double totalAmount = 0;
+        for (Rental each: _rentals)
+            totalAmount += each.getAmount();
+        return totalAmount;
+    }
 }
